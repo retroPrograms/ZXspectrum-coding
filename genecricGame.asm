cc3ee9a3a3cc736b637f69cd54bbad251bd57f72
retroPrograms <mcarvalho_au@hotmail.com>
2021-02-07 21:29:33 +1100

    Added setxy()

--- genecricGame.asm
+++ genecricGame.asm
@@ -11,9 +11,43 @@
 
 	call 3503 ; ROM routine - clears screen, opens chan 2.
 
+	call moveShip
+	call setxy
+
 	ld a,144  ; display chr 144
 	rst 16 
 
 	ret
 
+moveShip
+	ld hl, xcoord
+	ld c,31
+move_loop
+	ld (hl), c
+	ld a,144
+	rst 16
+	call delay
+	ld a, ' '
+	rst 16
+	dec c
+	jr nz, move_loop
+	
+	ret
+
+setxy 	ld a,22 ; ASCII control code for AT.
+	rst 16 ; print it.
+	ld a,(xcoord) ; vertical position.
+	rst 16 ; print it.
+	ld a,(ycoord) ; y coordinate.
+	rst 16 ; print it.
+	ret
+
+delay 	ld b,20 ; length of delay.
+delay0 	halt ; wait for an interrupt.
+	djnz delay0 ; loop.
+	ret ; return.
+
+
 blocks 	defb 16,16,56,56,124,124,254,254 ; player base
+xcoord 	defb 0
+ycoord 	defb 15
