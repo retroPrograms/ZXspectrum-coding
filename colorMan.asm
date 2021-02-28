89caf94477976fa9a66387622f4fb3daf89a3d35
retroPrograms <mcarvalho_au@hotmail.com>
2021-02-28 22:16:55 +1100

    Added clear_screen

--- colorMan.asm
+++ colorMan.asm
@@ -2,9 +2,9 @@ colorMem 	equ &5800
 colorSize 	equ 768
 
 
-	org &c350
+	org &8000
 
-	
+	call clear_screen
 	call drawMan
 	ret
 
@@ -27,6 +27,26 @@ dm_loop:
 	dec b			
 	jr nz,dm_loop2
 ret
+
+clear_screen:
+	push af
+	ld a,69
+	ld hl,&4000
+	ld bc,&17E0
+cl_loop:
+	ld a,69
+	ld (hl),a
+	inc hl
+	dec bc
+	ld a,c
+	cp 0
+	jr nz,cl_loop
+	ld a,b
+	cp 0
+	jr nz,cl_loop
+	pop af
+ret
+
 
 
 colorMan:
